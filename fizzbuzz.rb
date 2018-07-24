2bc63dcb1d5a5dd71f0092fc44c0698688af6666
Carolinemay <czhou@students.chccs.k12.nc.us>
2018-07-24 17:53:09 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,3 +2,9 @@
 #   if x is divisible by 3 return fizz
 #   if divisable by 5 return buzz 
 #     if not divisable by either return nothing 
+if x/3==0 && x/5==0
+  puts fizzbuzz
+elsif x/3==0 
+puts fizz
+elsif x/5 == 0
+puts buzz
